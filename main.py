0b89e6409710327e040113738a5e1e213c539339
tjarmane <charliehatch@gmail.com>
2017-09-06 21:50:07 +0000

    csv parsed in columns

--- main.py
+++ main.py
@@ -2,36 +2,35 @@ import os
 import math
 import urllib2
 import csv
-import numpy as np
+import matplotlib.pyplot as plt
+# from pylab import figure, axes, pie, title, show
+
+
 # retrieve davis .csv database
-url = 'http://biochar.ucdavis.edu/files/data/data.csv';
+# url = 'http://biochar.ucdavis.edu/files/data/data.csv';
 
-file_name = url.split('/')[-1]
-u = urllib2.urlopen(url)
-f = open(file_name, 'wb')
-meta = u.info()
-file_size = int(meta.getheaders("Content-Length")[0])
-print "Downloading: %s Bytes: %s" % (file_name, file_size)
+# file_name = url.split('/')[-1]
+# u = urllib2.urlopen(url)
+# f = open(file_name, 'wb')
+# meta = u.info()
+# file_size = int(meta.getheaders("Content-Length")[0])
+# print "Downloading: %s Bytes: %s" % (file_name, file_size)
 
-# downloading buffer
-file_size_dl = 0
-block_sz = 8192
-while True:
-    buffer = u.read(block_sz)
-    if not buffer:
-        break
+# # downloading buffer
+# file_size_dl = 0
+# block_sz = 8192
+# while True:
+#     buffer = u.read(block_sz)
+#     if not buffer:
+#         break
 
-    file_size_dl += len(buffer)
-    f.write(buffer)
-    status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-    status = status + chr(8)*(len(status)+1)
-    print status,
+#     file_size_dl += len(buffer)
+#     f.write(buffer)
+#     status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
+#     status = status + chr(8)*(len(status)+1)
+#     print status,
 
-f.close()
-
-# data = np.genfromtxt('data.csv', delimiter=',')
-# # Transpose data if needs be
-# data = np.transpose(data)
+# f.close()
 
 f = open("data.csv", 'rb')
 reader = csv.reader(f)
@@ -46,5 +45,3 @@ for row in reader:
         column[h].append(v)
 
 print headers
-
-print column['Biochar Name']
